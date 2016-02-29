af1777d56b9b777d8e2affbc17e00252da9be274
vamjan <janvampol@gmail.com>
2016-02-29 23:02:08 +0100

    Some folder rearanging and opening menu placeholder done...

---
+++ Assets/Scripts/MenuScripts/LoadOnClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public GameObject loadingImage;
+     
+    public void LoadScene(int level)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(level);
+    }
+}
---
+++ Assets/Scripts/SceneGenericScripts/ObjectPersistence.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPersistence : MonoBehaviour {
+
+	// Called before Start()
+	void Awake () {
+        DontDestroyOnLoad(gameObject);
+	}
+}
